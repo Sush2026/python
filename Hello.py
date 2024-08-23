a06f765d4cd9bee30ae9e072565607d2ffc13688
Sush2026 <ksushmiths487@gmail.com>
2024-08-23 17:06:52 +0530

    oops

--- Hello.py
+++ Hello.py
@@ -1,3 +1,22 @@
-print('Hello')
-print('Boy')
-print("OK")
+class Hi:
+    def __init__(self, a):
+        self.a = a
+
+    def __str__(self):
+        return f"{self.a}"
+    def display(self):
+        print(self.a)
+
+x = (int(input("enter the value")))
+
+obj = Hi(x)
+obj.display()
+print(obj)
+
+
+
+
+
+
+
+
